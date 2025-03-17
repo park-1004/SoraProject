b4278b504e9ef2fe9c64a4e7523ecf10b13e6f31
Sora <qkrwhddnr22@gmail.com>
2025-03-17 19:02:52 +0900

    7장 완료

---
+++ MyProject/src/ch07/sec07/exam03/Child.java
@@ -0,0 +1,15 @@
+package ch07.sec07.exam03;
+
+public class Child extends Parent{
+	public String field2;
+	
+	@Override
+	public void method2() {
+		System.out.println("Child-method2()");
+	}
+	
+	public void method3() {
+		System.out.println("Child-method3()");
+	}
+	
+}
